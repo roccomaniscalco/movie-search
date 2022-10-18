53842c27ec6b7719624459e4b303b61f566fb4a7
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-10-18 16:58:05 -0400

    improve perf of useCurrentBreakpoint

--- src/useCurrentBreakpoint.js
+++ src/useCurrentBreakpoint.js
@@ -1,13 +1,18 @@
 import { useMantineTheme } from "@mantine/core"
-import { useViewportSize } from "@mantine/hooks"
+import { useMediaQuery } from "@mantine/hooks"
 
 export default function useCurrentBreakpoint() {
   const theme = useMantineTheme()
-  const { width: viewportWidth } = useViewportSize()
+  const lessThanXs = useMediaQuery(`(max-width: ${theme.breakpoints.xs}px)`)
+  const lessThanSm = useMediaQuery(`(max-width: ${theme.breakpoints.sm}px)`)
+  const lessThanMd = useMediaQuery(`(max-width: ${theme.breakpoints.md}px)`)
+  const lessThanLg = useMediaQuery(`(max-width: ${theme.breakpoints.lg}px)`)
+  const lessThanXl = useMediaQuery(`(max-width: ${theme.breakpoints.xl}px)`)
 
-  const breakpointsAsEntries = Object.entries(theme.breakpoints)
-
-  return breakpointsAsEntries.find(
-    ([, width]) => width > viewportWidth
-  )?.[0] || "xxl"
+  if (lessThanXs) return "xs"
+  if (lessThanSm) return "sm"
+  if (lessThanMd) return "md"
+  if (lessThanLg) return "lg"
+  if (lessThanXl) return "xl"
+  return "xxl"
 }
